248a74be83667c0168948ed08b46f407546dac59
MrDave <davidschnoll@gmail.com>
2023-11-22 15:38:52 +0200

    Catch Timeout errors

--- main.py
+++ main.py
@@ -1,23 +1,35 @@
 import requests
 from pprint import pprint
 from environs import Env
+import logging
+
+
+def get_dvmn_response(token):
+    url = "https://dvmn.org/api/long_polling/"
+    headers = {
+        "Authorization": f"Token {token}"
+    }
+    response = requests.get(url, headers=headers, timeout=5)
+    response.raise_for_status()
+    return response.json()
 
 
 def main():
     env = Env()
     env.read_env()
 
-    devman_token = env.str("DEVMAN_AUTH")
-    url = "https://dvmn.org/api/long_polling/"
-    headers = {
-        "Authorization": f"Token {devman_token}"
-    }
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
+    logger = logging.getLogger()
+    logger.setLevel(level=logging.DEBUG)
 
-    pprint(response.json())
+    devman_token = env.str("DEVMAN_AUTH")
+
+    while True:
+        try:
+            response = get_dvmn_response(devman_token)
+            pprint(response)
+        except requests.exceptions.Timeout as error:
+            logging.debug(f"Timeout: {error}")
 
 
 if __name__ == '__main__':
-    while True:
-        main()
+    main()
